fb8752dd45407f6930266d84c49bd749a2118555
adanasir <adanasir@gmail.com>
2015-10-07 12:16:19 +0800

    python updated

---
+++ python/q1/test1q1.py
@@ -0,0 +1,16 @@
+#
+#SITI AMINAH SHUHADA MAT NASIR
+#2014433638
+#adanasir@gmail.com
+#github.com/adanasir
+
+
+with open("adanasirpy.bin", "wb") as file:
+    file.write(b"Name: Siti Aminah Shuhada Mat Nasir\n")
+    file.write(b"Matrix No.: 2014433638\n")
+    file.write(b"Gender: Female\n")
+    file.write(b"Age: 32 years old\n")
+    file.write(b"E-mails: adanasir@gmail.com\n")
+    file.write(b"Mobile Phone: 0199171336\n")
+file.close()
+
---
+++ python/q2/test1q2.py
@@ -0,0 +1,9 @@
+#TEST 1 QUESTION 2
+#SITI AMINAH SHUHADA MAT NASIR
+#2014433638
+#adanasir@gmail.com
+#github.com/adanasir
+
+fh = open("adanasirpy.bin", "rb")
+print (fh.read())
+fh.close
